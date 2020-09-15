cab40532400dad13786df9e6d5c46b577df83555
Nick Nobile <nickcnobile@gmail.com>
2020-09-15 01:19:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,14 +6,3 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
-
-def countdown_with_sleep(number)
-   number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-  sleep 5
-end
-"HAPPY NEW YEAR!"
-end
-end
-end
